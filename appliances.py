4fe5da52a8133d109367dc77b25b89eee32df127
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-08-30 10:33:46 -0500

    added appliances to a tuple

---
+++ appliances.py
@@ -0,0 +1,9 @@
+# @channel here's a lightning exercise to get the brain working this morning!
+# Appliances = (‘oven’, ‘refrigerator’, ‘coffee maker’, ‘rice cooker’)
+# Add 3 indispensable appliances to this tuple
+
+Appliances = ("oven", "refrigerator", "coffee maker", "rice cooker")
+
+Appliances = list(Appliances)
+Appliances.append(["blender", "toaster"])
+Appliances = tuple(Appliances)
